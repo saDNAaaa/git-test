588046add82e7b10bf3ac52e5e8b8b2dba7f5c33
wanao <wanao2008@hotmail.com>
2018-12-04 20:34:36 +0800

    0.0.1

---
+++ Makefile
@@ -0,0 +1,5 @@
+com: main.cpp
+	g++ -o main main.cpp
+run: main
+	chomd +x main
+	./main	
---
+++ main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+int main(){
+	cout << "MAKE" << endl;
+	cerr << "ERROR!\n";
+	return 0;
+}
